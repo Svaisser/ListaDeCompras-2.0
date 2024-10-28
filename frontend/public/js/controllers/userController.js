9b58074e5853ab11d0c7dfc2f7a70b011d23dc71
João Vitor Svaisser <svaisserjv@gmail.com>
2024-10-28 14:25:47 -0300

    Cadastro User funcionando

--- frontend/public/js/controllers/userController.js
+++ frontend/public/js/controllers/userController.js
@@ -1,3 +1,26 @@
 angular.module('meuApp').controller('userController', function ($scope, $http) {
 
+    $scope.frmUser = {
+        "name" : "",
+        "username" : "",
+        "password" : ""
+    }
+
+    $scope.createUser = function () {
+        $http.post('http://localhost:8080/users/', $scope.frmUser)
+           .then(function (response) {
+                console.log(response.data);
+            })
+           .catch(function (error) {
+                console.error('Erro:', error);
+            });
+
+        $scope.frmUser = {
+            "name" : "",
+            "username" : "",
+            "password" : ""
+        };
+
+        alert('Usuário criado com sucesso!');
+    }
 });
