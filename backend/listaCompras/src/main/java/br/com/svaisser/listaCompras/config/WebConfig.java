59a185acf554453139cac209a5faf95130328f56
João Vitor Svaisser <svaisserjv@gmail.com>
2024-10-28 13:54:47 -0300

    DESfazendo alguns erros do último commit

---
+++ backend/listaCompras/src/main/java/br/com/svaisser/listaCompras/config/WebConfig.java
@@ -0,0 +1,17 @@
+package br.com.svaisser.listaCompras.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica o CORS a todos os endpoints
+                .allowedOrigins("http://localhost:3000") // Permite requisições dessa origem
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite envio de cookies se necessário
+    }
+}
