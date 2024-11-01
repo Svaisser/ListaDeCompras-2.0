5118ede05de0ebd6348e73f0e2d991f175a747a7
João Vitor Svaisser <svaisserjv@gmail.com>
2024-11-01 14:34:04 -0300

    Update Server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
